71fccf9fd7cdf8fed2494054942786b4d9e4f336
Joshua Akan-Etuk <joshuaakanetuk@gmail.com>
2020-08-11 02:08:29 -0400

    init on Heroku

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const MOVIES = require('./movies-data.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -39,5 +40,15 @@ function getMovie(req, res) {
 
     res.json(results);
 }
+
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+})
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PORT = 3000
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
